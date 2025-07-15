84eff3c602c51792fe16158667014293ec6ee6ee
sren09 <svendyckmanns@hotmail.com>
2025-07-15 22:46:15 +0200

    Update __init__.py

--- custom_components/cupra_we_connect/__init__.py
+++ custom_components/cupra_we_connect/__init__.py
@@ -77,7 +77,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         _LOGGER,
         name=DOMAIN,
         update_method=async_update_data,
-        update_interval=timedelta(seconds=30),
+        update_interval=timedelta(minutes=10),
     )
 
     hass.data.setdefault(DOMAIN, {})
